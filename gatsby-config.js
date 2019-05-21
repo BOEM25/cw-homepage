e875a0ea919ab43a07e757593d12aaafefef42e5
Steve Castle <steve@tigerhaunt.com>
2019-05-20 19:23:58 -0700

    init netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Code Workshop`,
+    description: `Code Workshop is an organization delivering high quality coding workshops and adventure driven learning.`,
     author: `@gatsbyjs`,
   },
   plugins: [
