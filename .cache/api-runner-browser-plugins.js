ff35f7b2914613c0e83eb9619ecc8494f173b143
Steve Castle <steve@tigerhaunt.com>
2019-06-01 11:27:54 -0700

    init netlify cms starter

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/tracer/dev/cw-homepage/node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[],"maxWidth":2048},
+    },{
+      plugin: require('/Users/tracer/dev/cw-homepage/node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
+      options: {"plugins":[],"modulePath":"/Users/tracer/dev/cw-homepage/src/cms/cms.js"},
+    }]
