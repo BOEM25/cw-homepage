62bd8286b9e694093242658b7ff1d1d47df71933
Steve Castle <steve@tigerhaunt.com>
2019-07-10 21:00:47 -0700

    add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,20 @@
-var proxy = require('http-proxy-middleware')
+var proxy = require('http-proxy-middleware');
 
 module.exports = {
   siteMetadata: {
     title: 'Code Workshop',
     description:
-      'Code Workshop is a software development meetup hosted once monthly at the SYNShop, an awesome Las Vegas hacker space in Henderson, NV. It is open to people of all skill levels. See our event schedule below to find an event thats right for you..',
+      'Code Workshop is a software development meetup hosted once monthly at the SYNShop, an awesome Las Vegas hacker space in Henderson, NV. It is open to people of all skill levels. See our event schedule below to find an event thats right for you..'
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-68258529-3',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false
+      }
+    },
     {
       resolve: `gatsby-remark-prismjs`,
       options: {
@@ -50,22 +58,22 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/static/img`,
-        name: 'uploads',
-      },
+        name: 'uploads'
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
+        name: 'pages'
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images',
-      },
+        name: 'images'
+      }
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
@@ -76,8 +84,8 @@ module.exports = {
           {
             resolve: 'gatsby-remark-relative-images',
             options: {
-              name: 'uploads',
-            },
+              name: 'uploads'
+            }
           },
           {
             resolve: 'gatsby-remark-images',
@@ -85,32 +93,32 @@ module.exports = {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
+              maxWidth: 2048
+            }
           },
           {
             resolve: 'gatsby-remark-copy-linked-files',
             options: {
-              destinationDir: 'static',
-            },
-          },
-        ],
-      },
+              destinationDir: 'static'
+            }
+          }
+        ]
+      }
     },
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
     },
     {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
-      },
+        purgeOnly: ['/all.sass'] // applies purging only on the bulma css file
+      }
     }, // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
+    'gatsby-plugin-netlify' // make sure to keep it last in the array
   ],
   // for avoiding CORS while developing Netlify Functions locally
   // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
@@ -120,9 +128,9 @@ module.exports = {
       proxy({
         target: 'http://localhost:9000',
         pathRewrite: {
-          '/.netlify/functions/': '',
-        },
+          '/.netlify/functions/': ''
+        }
       })
-    )
-  },
-}
+    );
+  }
+};
