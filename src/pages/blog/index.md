f0760323c4bdec44d0c0dbb4b23eda5caca6d999
Stephen Castle <stephen@callsignmedia.com>
2019-08-11 10:26:32 -0700

    make blog page a template

---
+++ src/pages/blog/index.md
@@ -0,0 +1,8 @@
+---
+templateKey: 'blog-page'
+path: /blog
+title: Articles and Tutorials
+image: /img/writing.jpeg
+heading: Articles and Tutorials
+description: >-
+---
---
+++ src/templates/blog-page.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import BlogRoll from '../components/BlogRoll';
+
+export const BlogPageTemplate = ({
+  image,
+  title,
+  heading,
+  description,
+  intro
+}) => (
+  <div className="content">
+    <div
+      className="full-width-image-container margin-top-0"
+      style={{
+        backgroundImage: `url(${
+          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+        })`
+      }}
+    >
+      <h2
+        className="has-text-weight-bold is-size-1"
+        style={{
+          boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+          backgroundColor: '#f40',
+          color: 'white',
+          padding: '1rem'
+        }}
+      >
+        {title}
+      </h2>
+    </div>
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-7">
+              <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
+              <p>{description}</p>
+            </div>
+          </div>
+          <div className="columns">
+            <div className="column is-12">
+              <BlogRoll />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+);
+
+BlogPageTemplate.propTypes = {
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  title: PropTypes.string,
+  heading: PropTypes.string,
+  description: PropTypes.string,
+  intro: PropTypes.shape({
+    blurbs: PropTypes.array
+  }),
+  main: PropTypes.shape({
+    heading: PropTypes.string,
+    description: PropTypes.string,
+    image1: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    image2: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    image3: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
+  }),
+  testimonials: PropTypes.array,
+  fullImage: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
+};
+
+const BlogPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark;
+
+  return (
+    <Layout>
+      <BlogPageTemplate
+        image={frontmatter.image}
+        title={frontmatter.title}
+        heading={frontmatter.heading}
+        description={frontmatter.description}
+        intro={frontmatter.intro}
+        main={frontmatter.main}
+        testimonials={frontmatter.testimonials}
+        fullImage={frontmatter.full_image}
+        pricing={frontmatter.pricing}
+      />
+    </Layout>
+  );
+};
+
+BlogPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object
+    })
+  })
+};
+
+export default BlogPage;
+
+export const BlogPageQuery = graphql`
+  query VideoBlogPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 1920, quality: 80) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        heading
+        description
+      }
+    }
+  }
+`;
