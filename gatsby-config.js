2ddaffb944a79c24cc3c6457bbfb57413b0f1d11
Stephen Castle <stephen@callsignmedia.com>
2019-09-22 15:14:44 -0700

    fix block quote styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,6 +120,7 @@ module.exports = {
         whitelistPatternsChildren: [
           /^pre/,
           /language/,
+          /blockquote/,
           /line-numbers/,
           /token/,
           /highlight/
