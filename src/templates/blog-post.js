9981968788b2331da1f7871e058b4c610f541654
Stephen Castle <stephen@callsignmedia.com>
2020-01-05 12:45:24 -0800

    fix structured data typo

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -99,7 +99,7 @@ const BlogPost = ({ data }) => {
 "headline": "${post.frontmatter.title}",
 "publisher": {
   "@type": "Organization",
-  "name: "Code Workshop"
+  "name": "Code Workshop"
 },
 "image": "${siteUrl}${
               post.frontmatter.featuredimage.childImageSharp.fluid.src
