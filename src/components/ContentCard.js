a5a1970585675423bd9d2986185ea80d56a1dd07
Stephen Castle <stephen@callsignmedia.com>
2020-02-29 19:19:52 -0800

    update cards on tags pages

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { Link } from "gatsby";
 import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
+import { kebabCase } from "lodash";
 
 export default function ContentCard({ post }) {
   return (
     <div className="is-parent column is-4" key={post.id}>
-      <Link className="card-footer-item" to={post.fields.slug}>
-        <article
-          className={`card ${
-            post.frontmatter.featuredpost ? "is-featured" : ""
-          }`}
-        >
-          {post.frontmatter.featuredimage ? (
-            <div className="card-image">
+      <article
+        className={`card ${post.frontmatter.featuredpost ? "is-featured" : ""}`}
+      >
+        {post.frontmatter.featuredimage ? (
+          <Link className="" to={post.fields.slug}>
+            <div className="card-image slim-card-image">
               <PreviewCompatibleImage
                 imageInfo={{
                   image: post.frontmatter.featuredimage,
@@ -20,37 +19,47 @@ export default function ContentCard({ post }) {
                 }}
               />
             </div>
-          ) : null}
-          <div className="card-content">
-            <div className="media wrap">
-              <div class="media-left">
-                <figure class="image is-48x48">
-                  <PreviewCompatibleImage
-                    isRounded
-                    imageInfo={{
-                      image: post.frontmatter.authorimage,
-                      alt: `image of author for post ${post.title}`
-                    }}
-                  />
-                </figure>
-              </div>
-              <div className="media-content">
-                <h3 className="title is-4">{post.frontmatter.title}</h3>
-                <p className="subtitle is-6">{post.frontmatter.date}</p>
-              </div>
+          </Link>
+        ) : null}
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-48x48">
+                <PreviewCompatibleImage
+                  isRounded
+                  imageInfo={{
+                    image: post.frontmatter.authorimage,
+                    alt: `image of author for post ${post.title}`
+                  }}
+                />
+              </figure>
             </div>
 
-            <div className="content">
-              <p>{post.frontmatter.description}</p>
+            <div className="media-content">
+              <Link className="" to={post.fields.slug}>
+                <h3 className="title is-4">{post.frontmatter.title}</h3>
+              </Link>
+              <p className="subtitle is-6">{post.frontmatter.date}</p>
             </div>
           </div>
-          <footer class="card-footer">
-            <Link className="card-footer-item" to={post.fields.slug}>
-              Read More →
-            </Link>
-          </footer>
-        </article>
-      </Link>
+
+          <div className="content">
+            <p>{post.frontmatter.description}</p>
+          </div>
+          <div className="tags">
+            {post.frontmatter.tags.map(tag => (
+              <span className="tag is-danger" key={tag}>
+                <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+              </span>
+            ))}
+          </div>
+        </div>
+        <footer className="card-footer">
+          <Link className="card-footer-item" to={post.fields.slug}>
+            Read More →
+          </Link>
+        </footer>
+      </article>
     </div>
   );
 }
